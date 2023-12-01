c801478579dc20d505c9dfb474cef451ef7f4274
Thandi M. <TMenelas@outlook.com>
2023-12-01 08:07:17 -0500

    AoC 2022 Day 1: Part 2

--- Years/2022/1/index.js
+++ Years/2022/1/index.js
@@ -46,6 +46,46 @@ function part1() {
 }
 
 // Part 2
+function part2() {
+	// Get required modules
+	const fs = require('fs');
+	const path = require('path');
 
+	// Read input file
+	const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
 
+	// Parse input into array of numbers
+	const numbers = input.split('\n\n');
 
+	// Create array of total calories
+	const totalCalories = [];
+
+	// Iterate over the array of numbers, and calculate the total calories for each elf
+	for (let i = 0; i < numbers.length; i++) {
+		// Split the string into an array of numbers
+		const splitNumbers = numbers[i].split('\n');
+
+		// Calculate the total calories for the elf
+		let calories = 0;
+
+		for (let j = 0; j < splitNumbers.length; j++) {
+			calories += parseInt(splitNumbers[j]);
+		}
+
+		// Add the total calories to the array
+		totalCalories.push(calories);
+	}
+
+	// Sort the totalCalories array in descending order and return the sum of the first three elements
+	totalCalories.sort((a, b) => b - a);
+
+	// Get the three largest elements
+	const largest = totalCalories.slice(0, 3);
+
+	// Combine the three largest elements and return the result
+	console.log(largest)
+	return largest[0] + largest[1] + largest[2];
+}
+
+console.log('Part 1 Result: ' + part1());
+console.log('Part 2 Result: ' + part2());
