90118d899e64e3db2dd30407878feb9dfac32ebc
Thandi M. <TMenelas@outlook.com>
2023-12-01 07:32:19 -0500

    Code refactoring (whatever that means)

--- Years/2023/1/index.js
+++ Years/2023/1/index.js
@@ -5,6 +5,37 @@ function isNum(char) {
 	return char.match(/[0-9]/i);
 }
 
+// Part 1
+
+// Open a file called input1.txt
+fs.open('input.txt', 'r', (err, fd) => {
+	let totalNum = 0; // Total number
+	for (const line of fs.readFileSync(fd, 'utf8').split('\n')) {
+		// For each line, turn each string into a character array
+		const charArray = line.split('');
+
+		// For each array, make a separate array only including the numbers in order
+		const numArray = [];
+		for (let i = 0; i < charArray.length; i++) {
+			if (isNum(charArray[i])) {
+				numArray.push(charArray[i]);
+			}
+		}
+
+		// Take the first and last number and combine that into a number
+		const firstNum = numArray[0];
+		const lastNum = numArray[numArray.length - 1];
+		const combinedNum = parseInt(firstNum + lastNum);
+
+		// Add that number to the total
+		// console.log(combinedNum);
+		totalNum += combinedNum;
+	}
+	console.log(`Part 1 Result: ${totalNum}`);
+});
+
+// Part 2
+
 // Open a file called input.txt
 fs.open('input.txt', 'r', (err, fd) => {
 	let totalNum = 0;
@@ -36,5 +67,5 @@ fs.open('input.txt', 'r', (err, fd) => {
 		// Add that number to the total
 		totalNum += combinedNum;
 	}
-	console.log(totalNum);
+	console.log(`Part 2 Result: ${totalNum}`);
 });
