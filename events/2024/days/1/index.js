80cc4d2afb6213d7213205b3c79341d999531443
Thandi M. <TMenelas@outlook.com>
2024-12-02 14:13:33 -0500

    AoC 2024 Day 1 & 2: AOC IS HERE YIPPEEE

---
+++ events/2024/days/1/index.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const path = require('path');
+
+function part1() {
+	const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+	let result = 0;
+
+	const left = [];
+	const right = [];
+
+	for (const line of data.split('\n')) {
+		const [l, r] = line.split('   ');
+		left.push(l);
+		right.push(r);
+	}
+
+	const leftNum = left.map(Number);
+	leftNum.sort((a, b) => a - b);
+
+	const rightNum = right.map(Number);
+	rightNum.sort((a, b) => a - b);
+
+	const pairs = [];
+	for (let i = 0; i < leftNum.length; i++) pairs.push([leftNum[i], rightNum[i]]);
+
+	for (const [l, r] of pairs) result += Math.abs(l - r);
+
+	return result;
+}
+
+function part2() {
+	const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+	let result = 0;
+
+	const left = [];
+	const right = [];
+
+	for (const line of data.split('\n')) {
+		const [l, r] = line.split('   ');
+		left.push(l);
+		right.push(r);
+	}
+
+	const leftNum = left.map(Number);
+	leftNum.sort((a, b) => a - b);
+
+	const rightNum = right.map(Number);
+	rightNum.sort((a, b) => a - b);
+
+	const leftCount = {};
+	const rightCount = {};
+
+	for (const num of leftNum) leftCount[num] = (leftCount[num] || 0) + 1;
+	for (const num of rightNum) rightCount[num] = (rightCount[num] || 0) + 1;
+
+	for (const num of leftNum) if (rightCount[num]) result += num * rightCount[num];
+
+	return result;
+}
+
+console.log(`Part 1: ${part1()}`);
+console.log(`Part 2: ${part2()}`);
+
+module.exports = {
+	part1,
+	part2,
+};
