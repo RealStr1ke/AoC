9e8a5f47375df3e38027af4618eee40b7d6cdf0c
Thandi Menelas <TMenelas@outlook.com>
2024-12-03 11:25:18 -0500

    AoC 2024 Day 3: Rewrote it using regex

--- events/2024/days/3/index.js
+++ events/2024/days/3/index.js
@@ -5,68 +5,16 @@ function part1() {
 	const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
 	let result = 0;
 
-	const memory = [];
-	const pairs = [];
-	for (const line of data.split('\n')) memory.push(line.split(''));
+	const memory = data.split('\n').join('');
+	const match = /mul\((\d+),(\d+)\)/g;
+	const matches = memory.match(match);
 
-	for (const line of memory) {
-		for (let i = 0; i < line.length; i++) {
-			// console.log('CURRENT i' + i);
-			if (line[i] + line[i + 1] + line[i + 2] + line[i+3] === 'mul(') {
-				// console.log(i);
+	for (const pair of matches) {
+		const numbers = pair.slice(4, -1).split(',');
+		numbers[0] = parseInt(numbers[0]);
+		numbers[1] = parseInt(numbers[1]);
 
-				// console.log('found mul( at' + i);
-				// find the next full number
-				const start = i + 4;
-				let end = start;
-				const startChar = line[start];
-				let endChar = line[end];
-				// console.log(startChar);
-				// if startChar is a number, contunue
-				if (startChar >= 0 && startChar <= 9) {
-					while (endChar >= 0 && endChar <= 9) {
-						end++;
-						endChar = line[end];
-					}
-
-					// get the number
-					const number = parseInt(line.slice(start, end).join(''));
-					// console.log(number)
-
-					// find the next number
-					if (line[end] === ',') {
-						// console.log(number + line[end])
-						const secondStart = end + 1;
-						let secondEnd = secondStart;
-						const secondStartChar = line[secondStart];
-						let secondEndChar = line[secondEnd];
-
-						if (secondStartChar >= 0 && secondStartChar <= 9) {
-							while (secondEndChar >= 0 && secondEndChar <= 9) {
-								secondEnd++;
-								secondEndChar = line[secondEnd];
-							}
-
-							const secondNumber = parseInt(line.slice(secondStart, secondEnd).join(''));
-							// console.log(secondNumber)
-
-							if (line[secondEnd] === ')') {
-								// get the pair
-								const pair = [number, secondNumber];
-								// console.log(pair);
-								pairs.push(pair);
-							}
-
-						}
-					}
-				}
-
-			}
-		}
-	}
-
-	for (const pair of pairs) {
-		result += pair[0] * pair[1];
+		result += numbers[0] * numbers[1];
 	}
 
 	return result;
@@ -76,74 +24,23 @@ function part2() {
 	const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
 	let result = 0;
 
-	const memory = [];
-	const pairs = [];
-	for (const line of data.split('\n')) memory.push(line.split(''));
+	const memory = data.split('\n').join('');
+	const match = /mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g;
+	const matches = memory.match(match);
 
 	let enabled = true;
-	for (const line of memory) {
-		for (let i = 0; i < line.length; i++) {
-			// console.log('CURRENT i' + i);
-			
-			if (line[i] + line[i + 1] + line[i + 2] + line[i+3] === 'do()') {
-				enabled = true;
-			} else if (line[i] + line[i + 1] + line[i + 2] + line[i+3] + line[i+4] + line[i+5] + line[i+6] === 'don\'t()') {
-				enabled = false;
-			} else if (line[i] + line[i + 1] + line[i + 2] + line[i+3] === 'mul(' && enabled) {
-				// console.log(i);
+	for (const mem of matches) {
+		if (mem === 'do()') {
+			enabled = true;
+		} else if (mem === 'don\'t()') {
+			enabled = false;
+		} else if (mem.includes('mul(') && enabled) {
+			const numbers = mem.slice(4, -1).split(',');
+			numbers[0] = parseInt(numbers[0]);
+			numbers[1] = parseInt(numbers[1]);
 
-				// console.log('found mul( at' + i);
-				// find the next full number
-				const start = i + 4;
-				let end = start;
-				const startChar = line[start];
-				let endChar = line[end];
-				// console.log(startChar);
-				// if startChar is a number, contunue
-				if (startChar >= 0 && startChar <= 9) {
-					while (endChar >= 0 && endChar <= 9) {
-						end++;
-						endChar = line[end];
-					}
-
-					// get the number
-					const number = parseInt(line.slice(start, end).join(''));
-					// console.log(number)
-
-					// find the next number
-					if (line[end] === ',') {
-						// console.log(number + line[end])
-						const secondStart = end + 1;
-						let secondEnd = secondStart;
-						const secondStartChar = line[secondStart];
-						let secondEndChar = line[secondEnd];
-
-						if (secondStartChar >= 0 && secondStartChar <= 9) {
-							while (secondEndChar >= 0 && secondEndChar <= 9) {
-								secondEnd++;
-								secondEndChar = line[secondEnd];
-							}
-
-							const secondNumber = parseInt(line.slice(secondStart, secondEnd).join(''));
-							// console.log(secondNumber)
-
-							if (line[secondEnd] === ')') {
-								// get the pair
-								const pair = [number, secondNumber];
-								// console.log(pair);
-								pairs.push(pair);
-							}
-
-						}
-					}
-				}
-
-			}
+			result += numbers[0] * numbers[1];
 		}
-	}
-
-	for (const pair of pairs) {
-		result += pair[0] * pair[1];
 	}
 
 	return result;
