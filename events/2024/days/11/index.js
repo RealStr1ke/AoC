4cbec083a5a6c9395ebfe12a81babe0004f0f7cf
Thandi Menelas <TMenelas@outlook.com>
2024-12-11 16:09:12 -0500

    AoC 2024 Day 11: Commented my code for the blink function

--- events/2024/days/11/index.js
+++ events/2024/days/11/index.js
@@ -1,21 +1,41 @@
 import fs from 'fs';
 import path from 'path';
 
+/**
+ * Solution I found for Part 2: || Don't brute force this if you have any sort of empathy for your CPU lol, the numbers blow up pretty quickly. Basically what I did was instead of having an array/list of all the rocks as shown in the input, I made an object/json where the keys were the numbers that are on the rocks and the values were how many rocks had that number. Using that, with each iteration you can create a new object based on the one you had previously using the rules:
+ * - For n stones with the number 0, add n stones to the count of stones with the number 1.
+ * - For n stones with even length, split the number in half and add n stones to the count of stones with each half.
+ * - For n stones that don't follow those rules, add n stones to the count of stones with the number of the original stone multiplied by 2024.
+ * This approach optimizes the process by reducing the complexity from handling individual stones to managing counts of each unique stone value, thereby preventing the exponential growth of the stone list.
+ *
+ * @param {string[]} stones - An array of stone identifiers as strings.
+ * @param {number} iterations - The number of transformation iterations to perform.
+ * @returns {number} The total count of stones after all iterations have been completed.
+ */
 function blink(stones, iterations) {
+	// An object to store the count of each stone value. Changes each iteration.
 	let stonesObj = {};
+
+	// The cache that stores the transformed values of each stone. Used to optimize the process.
 	const cache = {};
 
+	// Initialize the stones object with the initial stone counts.
 	for (const stone of stones) {
 		if (!stonesObj[stone]) stonesObj[stone] = 0;
 		stonesObj[stone]++;
 	}
 
+	// Perform the specified number of iterations.
 	for (let i = 0; i < iterations; i++) {
+		// Create a blank object to store the new stone counts.
 		const newStonesObj = {};
 
+		// Check each stone in the current object (checks the keys of the object)
 		for (const stone of Object.keys(stonesObj)) {
+			// Get the count of the current stone.
 			const currentCount = stonesObj[stone];
 
+			// If the stone hasn't been cached yet, cache it.
 			if (!cache[stone]) {
 				if (stone === '0') {
 					cache[stone] = 1;
@@ -29,25 +49,36 @@ function blink(stones, iterations) {
 				// console.log(`[${stone}] -> [${cache[stone]}]`);
 			}
 
+
+			// Add the transformed stones to the new object.
 			if (stone.length % 2 === 0) {
+				// If either of the two halves aren't in the new object, add them.
 				if (!newStonesObj[cache[stone][0]]) newStonesObj[cache[stone][0]] = 0;
 				if (!newStonesObj[cache[stone][1]]) newStonesObj[cache[stone][1]] = 0;
+
+				// Add the current count to both halves.
 				newStonesObj[cache[stone][0]] += currentCount;
 				newStonesObj[cache[stone][1]] += currentCount;
 			} else {
+				// If the transformed stone isn't in the new object, add it.
 				if (!newStonesObj[cache[stone]]) newStonesObj[cache[stone]] = 0;
+
+				// Add the current count to the transformed stone.
 				newStonesObj[cache[stone]] += currentCount;
 			}
 
+			// Probably not necessary, but any stones in newStonesObj with a count of 0 are removed here
 			for (const s in newStonesObj) {
 				if (newStonesObj[s] === 0) delete newStonesObj[s];
 			}
 		}
 
+		// After iterating through all stones, the new object is saved to the actual stones object.
 		stonesObj = newStonesObj;
 	}
 
 
+	// After all iterations, for each stone in the object (checking object keys), add its count to the total stone count.
 	let stoneCount = 0;
 	for (const s of Object.keys(stonesObj)) {
 		stoneCount += stonesObj[s];
@@ -80,7 +111,6 @@ function blink(stones, iterations) {
 	}
 	return newStones;
 	 */
-
 }
 
 function part1() {
